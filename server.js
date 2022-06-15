3b06d2ccd0d8bb1895d65821e67778281ab70ce6
Saul Ruvalcaba <joshuarvlcb@gmail.com>
2022-06-15 12:01:37 -0700

    fixed L11 error

--- server.js
+++ server.js
@@ -48,7 +48,6 @@ nextApp.prepare().then(() => {
     if (err) return console.log(err, "foo");
 
     server.on("clientError", (err, socket) => {
-      console.error(err);
       socket.end("HTTP/1.1 400 Bad Request\r\n\r\n");
     });
     console.log(`listening to port ${port}`);
