42f96fec63c65c4d5f1b921dde12a2b5f02ebe91
Saul Ruvalcaba <joshuarvlcb@gmail.com>
2022-06-14 21:01:34 -0700

    changed the base URL

--- util/auth.js
+++ util/auth.js
@@ -1,7 +1,7 @@
 import Cookie from "js-cookie";
 import Router from "next/router";
 
-export const baseURL = "http://localhost:3000/api/v1/";
+export const baseURL = "https://snickerz.herokuapp.com/";
 
 export const addingCookie = (token) => {
   Cookie.set("token", token);
